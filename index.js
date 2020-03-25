83d7e24969ba03b9314291e52e6238c47f3a04cf
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-03-25 14:07:00 -0700

    Baby constructor task complete, just need to finish car task

--- index.js
+++ index.js
@@ -85,8 +85,8 @@ Car.prototype.fill = function (gallons) {
 }
 
 Car.prototype.drive = function (distance) {
-  this.odometer = this.odometer += distance;
-  this.tank -= (distance / this.milesPerGallon);
+  this.odometer = this.odometer + distance;
+  this.tank = this.tank - (distance / this.milesPerGallon);
   if (this.tank*this.milesPerGallon >= distance) {
     return `I ran out of fuel at ${this.odometer} miles!`
   }
@@ -100,9 +100,16 @@ Car.prototype.drive = function (distance) {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+Person.call(this, name, age);
+this.favoriteToy = favoriteToy;
 }
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function () {
+  return `Playing with ${this.favoriteToy}`
+}
+
+
 
 /* 
   TASK 4
